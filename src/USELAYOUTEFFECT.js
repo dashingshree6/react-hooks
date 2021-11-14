4fb83e49e4f1b9375e8418e9eddfb3c2064a4c2f
dashingshree6 <dashingshree6@gmail.com>
2021-11-14 22:52:57 +0530

    Create USELAYOUTEFFECT.js

---
+++ src/USELAYOUTEFFECT.js
@@ -0,0 +1,50 @@
+import React, { useLayoutEffect, useState } from "react";
+import axios from "axios";
+import { Button, Icon, Image, Item, Label } from "semantic-ui-react";
+
+export default function USELAYOUTEFFECT() {
+  const [data, setData] = useState("");
+  const [price, setPrice] = useState("");
+  const [desc, setDesc] = useState("");
+  const [category, setCategory] = useState("");
+  const [image, setImage] = useState("");
+  const [rating, setRating] = useState("");
+
+  useLayoutEffect(() => {
+    axios.get("https://fakestoreapi.com/products").then((response) => {
+      setImage(response.data[0].image);
+      setData(response.data[0].title);
+      setPrice(response.data[0].price);
+      setDesc(response.data[0].description);
+      setCategory(response.data[0].category);
+      console.log(data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h2>useLayoutEffect Hook</h2>
+      <Item.Group>
+        <Item>
+          <Item.Image size="tiny" src={image} />
+
+          <Item.Content>
+            <Item.Header as="a">{data}</Item.Header>
+            <Item.Meta>
+              <span className="cinema">{category}</span>
+            </Item.Meta>
+            <Item.Description>{desc}</Item.Description>
+            <Item.Extra>
+              <Button primary floated="right">
+                Buy Now
+                <Icon name="right chevron" />
+              </Button>
+              <Label floated="right">{price}</Label>
+            </Item.Extra>
+          </Item.Content>
+        </Item>
+      </Item.Group>
+      <br />
+    </div>
+  );
+}
